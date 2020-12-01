5bbe067d4e2886adc9e8d5bb3246966e6b77af58
Relintai <relintai@gmail.com>
2020-12-01 14:46:08 +0100

    Bit more work on the database interface mockup.

--- core/database.h
+++ core/database.h
@@ -15,14 +15,17 @@ public:
 class Database {
 public:
 	//probably needs refcount, depending on what db engines do, todo
-	virtual QueryResult *query(const std::string &sql);
-	virtual QueryResult *query_async(const std::string &sql);
+	//virtual QueryResult *query(const std::string &sql);
+	//virtual QueryResult *query_async(const std::string &sql);
 	//or
-	virtual QueryErrorCode query(QueryResult *result, const std::string &sql);
-	virtual QueryErrorCode query_async(QueryResult *result, const std::string &sql);
+	//virtual QueryErrorCode query(QueryResult *result, const std::string &sql);
+	//virtual QueryErrorCode query_async(QueryResult *result, const std::string &sql);
 
 	//also
 	//virtual QueryResult *query_prepared(const std::string &sql, param1, param2, ...);
+
+    //query interface (codeigniter 3 style)
+    //virtual void where(""); etc
 
 	Database();
 	~Database();
