ba90610e3e6293bd3b16fe6b68664ed4b22cefa3
Relintai <relintai@gmail.com>
2020-12-27 03:36:56 +0100

    Removed bbcpp.

--- core/utils.h
+++ core/utils.h
@@ -3,9 +3,6 @@
 
 #include <string>
 
-#include <bbcpp/BBDocument.h>
-
-
 class Utils {
     public:
     static void newline_to_br(std::string *str);
@@ -13,13 +10,8 @@ class Utils {
 
     static void markdown_to_html(std::string *str);
 
-    static void bbcode_evaluate_simple(std::string *str);
-    static bbcpp::BBDocumentPtr bbcode(const std::string &str);
-
     static void str_replace(std::string *str, const std::string& from, const std::string& to);
 protected:
-    static void eval_node(std::string *str, bbcpp::BBNodePtr node);
-    static void eval_element(std::string *str, bbcpp::BBElementPtr node);
 };
 
 
