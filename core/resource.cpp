af37189fcf38ac5fc365e9d436a69b65a754e1f8
Relintai <relintai@protonmail.com>
2021-08-21 14:40:30 +0200

    Initial Resource implementation.

--- core/resource.cpp
+++ core/resource.cpp
@@ -1,9 +1,116 @@
 #include "resource.h"
 
-Resource::Resource() {
+#if DATABASES_ENABLED
+#include "core/database/database.h"
+#include "core/database/database_manager.h"
+#endif
 
+int Resource::get_id() {
+	return _id;
+}
+void Resource::set_id(const int value) {
+	_id = value;
+
+	changed();
+}
+
+bool Resource::get_dirty() {
+	return _dirty;
+}
+void Resource::set_dirty(const bool value) {
+	_dirty = value;
+}
+
+std::string Resource::get_resource_name() {
+	return _resource_name;
+}
+void Resource::set_resource_name(const std::string &name) {
+	_resource_name = name;
+
+	changed();
+}
+
+void Resource::changed() {
+	_dirty = true;
+}
+
+void Resource::save() {
+#if DATABASES_ENABLED
+	sql_save();
+#else
+	file_save();
+#endif
+}
+void Resource::load() {
+#if DATABASES_ENABLED
+	sql_load();
+#else
+	file_load();
+#endif
+}
+void Resource::migrate() {
+#if DATABASES_ENABLED
+	//todo!
+
+	sql_delete_tables();
+	sql_create_tables();
+
+	//sql_migrate();
+#else
+	file_ensure_directory_exist();
+#endif
+}
+
+#if DATABASES_ENABLED
+void Resource::sql_save() {
+	sql_save(DatabaseManager::get_singleton()->ddb);
+}
+void Resource::sql_load() {
+	sql_load(DatabaseManager::get_singleton()->ddb);
+}
+void Resource::sql_migrate() {
+	sql_migrate(DatabaseManager::get_singleton()->ddb);
+}
+void Resource::sql_create_tables() {
+	sql_create_tables(DatabaseManager::get_singleton()->ddb);
+}
+void Resource::sql_delete_tables() {
+	sql_delete_tables(DatabaseManager::get_singleton()->ddb);
+}
+
+void Resource::sql_save(Database *db) {
+}
+void Resource::sql_load(Database *db) {
+}
+void Resource::sql_migrate(Database *db) {
+}
+void Resource::sql_create_tables(Database *db) {
+}
+void Resource::sql_delete_tables(Database *db) {
+}
+#endif
+
+void Resource::file_save() {
+}
+void Resource::file_load() {
+}
+void Resource::file_ensure_directory_exist() {
+}
+std::string Resource::file_get_base_path() {
+	return "./resources/";
+}
+
+std::string Resource::to_json(rapidjson::Document *document) {
+	return "";
+}
+void Resource::from_json(const std::string &data) {
+}
+
+Resource::Resource() {
+	_id = 0;
+	_dirty = false;
+	_resource_name = get_class();
 }
 
 Resource::~Resource() {
-
 }
--- core/resource.h
+++ core/resource.h
@@ -1,18 +1,65 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <string>
+
+#include "libs/rapidjson/document.h"
 #include "reference.h"
+
+#if DATABASES_ENABLED
+class Database;
+#endif
 
 class Resource : public Reference {
 	RCPP_OBJECT(Resource, Reference);
 
 public:
+	int get_id();
+	void set_id(const int value);
+
+	bool get_dirty();
+	void set_dirty(const bool value);
+
+	std::string get_resource_name();
+	void set_resource_name(const std::string &name);
+
+	void changed();
+
+	virtual void save();
+	virtual void load();
+	virtual void migrate();
+
+#if DATABASES_ENABLED
+	virtual void sql_save();
+	virtual void sql_load();
+	virtual void sql_migrate();
+	virtual void sql_create_tables();
+	virtual void sql_delete_tables();
+
+	virtual void sql_save(Database *db);
+	virtual void sql_load(Database *db);
+	virtual void sql_migrate(Database *db);
+	virtual void sql_create_tables(Database *db);
+	virtual void sql_delete_tables(Database *db);
+#endif
+
+	virtual void file_save();
+	virtual void file_load();
+	virtual void file_ensure_directory_exist();
+	virtual std::string file_get_base_path();
+
+	std::string to_json(rapidjson::Document *document = nullptr);
+	void from_json(const std::string &data);
+
+	//todo properties
 
 	Resource();
 	~Resource();
 
 private:
+	int _id;
+	bool _dirty;
+	std::string _resource_name;
 };
-
 
 #endif
