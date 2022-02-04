d42311356f3dc4b06ec5681b2d8c164c46058ee0
Relintai <relintai@protonmail.com>
2022-02-04 08:53:57 +0100

    Added a signal class.

---
+++ core/signal.cpp
@@ -0,0 +1,107 @@
+#include "signal.h"
+
+void Signal::connect_static(void (*func)(Signal *)) {
+	StaticSignalEntry *se = new StaticSignalEntry();
+	se->func = func;
+
+	entries.push_back(se);
+}
+void Signal::disconnect_static(void (*func)(Signal *)) {
+	for (int i = 0; i < entries.size(); ++i) {
+		SignalEntry *e = entries[i];
+
+		if (e->type == SIGNAL_ENTRY_TYPE_STATIC) {
+			StaticSignalEntry *se = static_cast<StaticSignalEntry *>(e);
+
+			if (se->func == func) {
+				entries.remove_keep_order(i);
+				return;
+			}
+		}
+	}
+}
+bool Signal::is_connected_static(void (*func)(Signal *)) {
+	for (int i = 0; i < entries.size(); ++i) {
+		SignalEntry *e = entries[i];
+
+		if (e->type == SIGNAL_ENTRY_TYPE_STATIC) {
+			StaticSignalEntry *se = static_cast<StaticSignalEntry *>(e);
+
+			if (se->func == func) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void Signal::emit() {
+	for (int i = 0; i < entries.size(); ++i) {
+		entries[i]->call(this);
+	}
+}
+
+void Signal::emit(const Variant &p1) {
+	params.push_back(p1);
+
+	for (int i = 0; i < entries.size(); ++i) {
+		entries[i]->call(this);
+	}
+
+	params.clear();
+}
+void Signal::emit(const Variant &p1, const Variant &p2) {
+	params.push_back(p1);
+	params.push_back(p2);
+
+	for (int i = 0; i < entries.size(); ++i) {
+		entries[i]->call(this);
+	}
+
+	params.clear();
+}
+void Signal::emit(const Variant &p1, const Variant &p2, const Variant &p3) {
+	params.push_back(p1);
+	params.push_back(p2);
+	params.push_back(p3);
+
+	for (int i = 0; i < entries.size(); ++i) {
+		entries[i]->call(this);
+	}
+
+	params.clear();
+}
+
+void Signal::emit(const Variant &p1, const Variant &p2, const Variant &p3, const Variant &p4) {
+	params.push_back(p1);
+	params.push_back(p2);
+	params.push_back(p3);
+	params.push_back(p4);
+
+	for (int i = 0; i < entries.size(); ++i) {
+		entries[i]->call(this);
+	}
+
+	params.clear();
+}
+
+void Signal::emit(const Variant &p1, const Variant &p2, const Variant &p3, const Variant &p4, const Variant &p5) {
+	params.push_back(p1);
+	params.push_back(p2);
+	params.push_back(p3);
+	params.push_back(p4);
+	params.push_back(p5);
+
+	for (int i = 0; i < entries.size(); ++i) {
+		entries[i]->call(this);
+	}
+
+	params.clear();
+}
+
+Signal::Signal() :
+		Object() {
+}
+Signal::~Signal() {
+}
