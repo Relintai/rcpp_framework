8dda820a54d58ea3164c8816c6f6480fad2badf5
Relintai <relintai@protonmail.com>
2022-02-04 10:37:36 +0100

    Renamed Color to ColorUInt8, and typedef-ed it to Color.

--- core/math/color.h
+++ core/math/color.h
@@ -1,13 +1,17 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "core/typedefs.h"
 #include <inttypes.h>
 
 #ifdef SDL_AVAILABLE
 #include <SDL.h>
 #endif
 
-class Color {
+//Todo add float version
+//Not sure but probably that should be the default
+
+class ColorUInt8 {
 public:
 	uint32_t to_key() const;
 	void from_key(const uint32_t key);
@@ -17,15 +21,29 @@ public:
 	void from_sdl_color(const SDL_Color &key);
 #endif
 
-	Color();
-	Color(uint8_t p_r, uint8_t p_g, uint8_t p_b, uint8_t p_a = 255);
-	Color(const uint32_t key);
-	virtual ~Color();
+	_FORCE_INLINE_ uint8_t &operator[](int idx) {
+		return components[idx];
+	}
+	_FORCE_INLINE_ const uint8_t &operator[](int idx) const {
+		return components[idx];
+	}
 
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-	uint8_t a;
+	ColorUInt8();
+	ColorUInt8(uint8_t p_r, uint8_t p_g, uint8_t p_b, uint8_t p_a = 255);
+	ColorUInt8(const uint32_t key);
+	virtual ~ColorUInt8();
+
+	union {
+		struct {
+			uint8_t r;
+			uint8_t g;
+			uint8_t b;
+			uint8_t a;
+		};
+		uint8_t components[4];
+	};
 };
+
+typedef ColorUInt8 Color;
 
 #endif
