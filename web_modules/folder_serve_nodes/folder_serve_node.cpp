c59248435068032a360c21dead6731cf53886672
Relintai <relintai@protonmail.com>
2022-02-06 09:21:01 +0100

    Added a new FolderServeNode.

---
+++ web_modules/folder_serve_nodes/folder_serve_node.cpp
@@ -0,0 +1,56 @@
+#include "folder_serve_node.h"
+
+#include "web/file_cache.h"
+#include "web/html/html_builder.h"
+#include "web/http/request.h"
+
+void FolderServeNode::handle_request_main(Request *request) {
+	const String &rp = request->get_current_path_segment();
+
+	if (rp == "") {
+		request->send_error(HTTP_STATUS_CODE_404_NOT_FOUND);
+		return;
+	}
+
+	String file_name = "/" + request->get_path();
+
+	if (file_cache->wwwroot_has_file(file_name)) {
+		String fp = file_cache->wwwroot;
+		fp.append_path(file_name);
+
+		request->send_file(fp);
+		return;
+	}
+
+	if (!try_route_request_to_children(request)) {
+		_handle_request_main(request);
+	}
+}
+
+void FolderServeNode::load() {
+	file_cache->clear();
+
+	if (serve_folder == "") {
+		return;
+	}
+
+	file_cache->wwwroot = serve_folder;
+	file_cache->wwwroot_refresh_cache();
+}
+
+void FolderServeNode::_notification(const int what) {
+	switch (what) {
+		case NOTIFICATION_ENTER_TREE:
+			load();
+			break;
+		default:
+			break;
+	}
+}
+
+FolderServeNode::FolderServeNode() :
+		WebNode() {
+}
+
+FolderServeNode::~FolderServeNode() {
+}
